acfabdb702de9ab8bd8f18314b4e001b20ed1536
Francois Marier <francois@mozilla.com>
2016-09-21 06:50:42 +0100

    Disable Navigation Timing API
    https://www.w3.org/TR/navigation-timing/

--- user.js
+++ user.js
@@ -19,6 +19,7 @@ user_pref("pdfjs.disabled", true); // use external PDF viewer instead
 user_pref("layout.css.visited_links_enabled", false); // regularly leaks
 user_pref("media.video_stats.enabled", false);
 user_pref("browser.casting.enabled", false); // https://bugzil.la/1111967
+user_pref("dom.enable_performance", false);
 
 //////////////////////////
 // Features to restrict
