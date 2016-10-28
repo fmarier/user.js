0683aad04967b77dada05557f5b20635ba321c6f
Francois Marier <francois@mozilla.com>
2016-10-28 10:30:07 -0700

    Fx 52 already defaults to sha1_enforcement_level=4
    http://searchfox.org/mozilla-central/rev/445097654c1cc1098ee0171a29c439afe363a588/netwerk/base/security-prefs.js#59

--- user.js
+++ user.js
@@ -34,7 +34,6 @@ user_pref("media.peerconnection.ice.default_address_only", true); // Firefox < 5
 user_pref("media.peerconnection.ice.no_host", true); // Firefox >= 51
 // Turn off SHA1 certificates issued in 2016 or later
 user_pref("security.pki.sha1_enforcement_level", 2); // Firefox < 52
-user_pref("security.pki.sha1_enforcement_level", 4); // Firefox >= 52
 
 // Don't send cross-origin referrers
 // https://feeding.cloud.geek.nz/posts/tweaking-referrer-for-privacy-in-firefox/
