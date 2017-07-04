8c9275628b1a908092525198aa064a6bf26655a6
Francois Marier <francois@mozilla.com>
2017-07-03 17:06:53 -0700

    Fix UX annoyance (closing last tab by mistake)

--- user.js
+++ user.js
@@ -62,5 +62,8 @@ user_pref("geo.wifi.uri", "https://location.services.mozilla.com/v1/geolocate?ke
 // Recommended by https-by-default add-on
 user_pref("browser.urlbar.trimURLs", false);
 
+// Prevent accidental closure of browser windows
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+
 // Automatically expand the "more details" part of TLS errors
 user_pref("browser.xul.error_pages.expert_bad_cert", true);
